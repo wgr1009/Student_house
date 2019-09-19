c5a1e319fd9185165ca7dc5ccf7557b86b1e5f23
wgr1009 <wgr_1009@163.com>
2019-09-20 07:24:00 +0800

    修改views函数为 类函数，编写中间件模块

--- student_sys/student/urls.py
+++ student_sys/student/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from .views import IndexView
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('index/', IndexView.as_view(), name='index'),
 ]
--- student_sys/student/views.py
+++ student_sys/student/views.py
@@ -1,36 +1,68 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.views import  View
 
 from .models import *
 from .forms import StudentForm
 
 
-def index(request):
-    # students = Student.objects.all()
-    students = Student.get_all()  # 因为后期可能会对学员的数据 进行过滤，比如需要调整为展示所有审核通过的学员， 此时就需要调整 view 函数中的代码者对结果进行缓存，也需要调整代码
-    if request.method == 'POST':
+# def index(request):
+#     # students = Student.objects.all()
+#     students = Student.get_all()  # 因为后期可能会对学员的数据 进行过滤，比如需要调整为展示所有审核通过的学员， 此时就需要调整 view 函数中的代码者对结果进行缓存，也需要调整代码
+#     if request.method == 'POST':
+#         form = StudentForm(request.POST)
+#         if form.is_valid():
+#             # cleaned_data = form.cleaned_data   手动构建 Studnet 对象 保存 Student数据
+#             # student = Student()
+#             # student.name = cleaned_data['name']
+#             # student.sex = cleaned_data['sex']
+#             # student.email = cleaned_data['email']
+#             # student.profession = cleaned_data['profession']
+#             # student.qq = cleaned_data['qq']
+#             # student.phone = cleaned_data['phone']
+#             # student.save()
+#
+#             form.save()
+#             return HttpResponseRedirect(reverse('index'))
+#     else:
+#         form = StudentForm()
+#
+#     context = {
+#         'students': students,
+#         'form': form
+#     }
+#     # 如果是 get 请求 返回空  post填充
+#     return render(request, 'index.html', context=context)
+
+
+
+class IndexView(View):
+    template_name = 'index.html'
+
+    def get_context(self):
+        students = Student.get_all()
+        context = {
+            'students': students
+        }
+        return context
+
+    def get(self, request):
+        context = self.get_context()
+        form = StudentForm()
+        context.update({
+            'form': form
+        })
+
+        return render(request, self.template_name, context=context)
+
+    def post(self, request):
         form = StudentForm(request.POST)
         if form.is_valid():
-            # cleaned_data = form.cleaned_data   手动构建 Studnet 对象 保存 Student数据
-            # student = Student()
-            # student.name = cleaned_data['name']
-            # student.sex = cleaned_data['sex']
-            # student.email = cleaned_data['email']
-            # student.profession = cleaned_data['profession']
-            # student.qq = cleaned_data['qq']
-            # student.phone = cleaned_data['phone']
-            # student.save()
-
             form.save()
             return HttpResponseRedirect(reverse('index'))
-    else:
-        form = StudentForm()
-
-    context = {
-        'students': students,
-        'form': form
-    }
-    # 如果是 get 请求 返回空  post填充
-    return render(request, 'index.html', context=context)
-
+        context = self.get_context()
+        context.update({
+            'form': form
+        })
+        return render(request, self.template_name, context=context)
